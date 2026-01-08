4ab587f999b1dafafcf9533136118ebe001b2800
khh-Niloy <khhniloy0@gmail.com>
2026-01-09 00:49:57 +0600

    fixed redis url

--- src/app/lib/connectRedis.ts
+++ src/app/lib/connectRedis.ts
@@ -1,4 +1,4 @@
 import Redis from "ioredis";
-// import { envVars } from "../config/env";
+import { envVars } from "../config/env";
 
-export const redis = new Redis();
+export const redis = new Redis(envVars.REDIS_URL);
